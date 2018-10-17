afbf4715cf615becac67d059ea8eb7e435011a98
jorgitoml <jorgito_ml@hotmail.com>
2018-10-17 21:10:14 +0200

    pre scroller swipe

--- src/components/MoviesSection/moviesSlider/moviesSlider.js
+++ src/components/MoviesSection/moviesSlider/moviesSlider.js
@@ -5,7 +5,9 @@ import arrow from '../../../assets/img/slider-arrow.png';
 class moviesSlider extends Component {
 
     state = {
-        position: 0
+        position: 0,
+        direction: 'next',
+        sliding: false
     }
 
     getOrder = itemIndex=>{
@@ -21,20 +23,38 @@ class moviesSlider extends Component {
     nextSlide = ()=>{
         const { position } = this.state;
         const numItems = this.props.movies.length || 1;
-        this.setState({
-          position: position === numItems - 1 ? 0 : position + 1
-        })
+        this.doSliding('next',position === numItems - 1 ? 0 : position + 1);
     }
 
-    prevSlide = () => {
+    prevSlide = ()=>{
         const { position } = this.state;
         const numItems = this.props.movies.length
+        this.doSliding('prev',position === 0 ? numItems - 1 : position - 1);
+    }
+
+    doSliding = (direction, position) => {
         this.setState({
-            position: position === 0 ? numItems - 1 : position - 1
-        })
+          sliding: true,
+          direction: direction,
+          position: position
+        });
+
+        setTimeout(() => {
+          this.setState({
+            sliding: false
+          })
+        }, 300)
     }
 
     render() {
+
+        const track_style = ['movies-slider__slot'];
+        this.state.sliding ? 
+            (this.state.direction==='next') ? track_style.push('sliding_track','sliding_track-next') : track_style.push('sliding_track','sliding_track-prev') 
+            : track_style.push('not-sliding_track');
+            
+        
+
         return (
             <div className="movies-slider">
                 
@@ -42,13 +62,15 @@ class moviesSlider extends Component {
                     <div className="movies-slider__container__previous-button" onClick={()=>this.prevSlide()}>
                         <img alt="next" src={arrow} />
                     </div>
-                    <div className="movies-slider__container__track">
+                    <div 
+                        className='movies-slider__container__track'
+                    >
                         {
                             this.props.movies.map((item,index)=>{
                                 return (
                                     <div 
                                         key={item.id} 
-                                        className="movies-slider__slot"
+                                        className={track_style.join(' ')}
                                         style={{order:this.getOrder(index)}}
                                         >
                                             <img src={item.images.artwork} />
